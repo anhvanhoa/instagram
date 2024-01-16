7bd3527ced30d898909e1646df4adec21f1c83d3
AnhVanHoa <anhhoccode@gmail.com>
2024-01-16 10:28:47 +0700

    config cookies

--- server/src/controllers/Auth.controller.ts
+++ server/src/controllers/Auth.controller.ts
@@ -49,7 +49,7 @@ class AuthController {
             const response = await authProvider.loginFacebook(body, (token) => {
                 res.cookie('tokenRefresh', token, {
                     httpOnly: true,
-                    sameSite: 'lax',
+                    sameSite: 'strict',
                     domain: process.env.URL_CLIENT,
                 })
             })
@@ -69,7 +69,7 @@ class AuthController {
                 res.cookie('tokenRefresh', token, {
                     httpOnly: true,
                     secure: true,
-                    sameSite: 'lax',
+                    sameSite: 'strict',
                     domain: process.env.URL_CLIENT,
                 })
             })
@@ -109,7 +109,7 @@ class AuthController {
             const response = await authProvider.refreshJwt(tokenRefresh, (token) => {
                 res.cookie('tokenRefresh', token, {
                     httpOnly: true,
-                    sameSite: 'lax',
+                    sameSite: 'strict',
                     domain: process.env.URL_CLIENT,
                 })
             })
