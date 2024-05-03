8b9b167c102477c1bf12b650abf6c9979419d63e
AnhVanHoa <anhhoccode@gmail.com>
2024-05-03 23:11:48 +0700

    change declare express

--- server/src/type.d.ts
+++ server/src/type.d.ts
@@ -1,27 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { BoxChat } from '~/types/boxChat'
-import { ContentChat, ResUsersChat } from '~/types/chat'
-import { Code } from '~/types/code'
-import { Comment } from '~/types/comment'
-import { SizeCrop } from '~/types/image'
-import { LoginFB, LoginType } from '~/types/login'
-import { NotificationSchema } from '~/types/notification'
-import { Post } from '~/types/post'
-import { Info, InfoType, Register, ResInfo } from '~/types/register'
-import { RoomChat } from '~/types/roomChat'
-import {
-    ClientToServerEvents,
-    DeleteChat,
-    DetailChat,
-    InterServerEvents,
-    NotificationEmit,
-    ServerToClientEvents,
-    SocketIo,
-} from '~/types/socket'
-import { Token } from '~/types/token'
+// import { BoxChat } from '~/types/boxChat'
+// import { ContentChat, ResUsersChat } from '~/types/chat'
+// import { Code } from '~/types/code'
+// import { Comment } from '~/types/comment'
+// import { SizeCrop } from '~/types/image'
+// import { LoginFB, LoginType } from '~/types/login'
+// import { NotificationSchema } from '~/types/notification'
+// import { Post } from '~/types/post'
+// import { Info, InfoType, Register, ResInfo } from '~/types/register'
+// import { RoomChat } from '~/types/roomChat'
+// import {
+//     ClientToServerEvents,
+//     DeleteChat,
+//     DetailChat,
+//     InterServerEvents,
+//     NotificationEmit,
+//     ServerToClientEvents,
+//     SocketIo,
+// } from '~/types/socket'
+// import { Token } from '~/types/token'
 import { User, Gender, ResUser, UserNoPassword } from '~/types/user'
 import { type Request, Express } from 'express'
-declare module 'express' {
+declare module '@types/express' {
     interface Express {}
     interface Request {
         user?: Omit<User, 'password'>
